a358d29a27c8ec7551bc08301abcc42f92510ea8
Md. Jobaidul Islam <jobaidul.cse.brur@gmail.com>
2026-04-04 01:11:02 +0600

    Initial commit with GitHub Actions UWP build

---
+++ AppLaunchRnd/App.xaml.cs
@@ -0,0 +1,54 @@
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MyApp
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            this.InitializeComponent();
+            stopwatch.Stop();
+            System.Diagnostics.Debug.WriteLine($"App.InitializeComponent() took: {stopwatch.ElapsedMilliseconds} ms");
+
+            this.Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                Window.Current.Activate();
+            }
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
